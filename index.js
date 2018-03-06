b15d7810cc13ded7eb22add05190a23ad1063e16
FallenXP <expgamer303@gmail.com>
2018-03-06 18:33:18 -0500

    v2.0.0
    More functions

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 exports.hello = function() {
     console.log("Hello!");
 }
+
+exports.hey = function() {
+    console.log("Hey!");
+}
+
+exports.helloWorld = function() {
+    console.log("Hello World!");
+}
+
+exports.welcome = function() {
+    console.log("Welcome!");
+}
